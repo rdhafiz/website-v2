ce5f1982b550ba40d5a5d19c0a84926f0ff11432
Ridwanul Hafiz <ridwanul.hafiz@gmail.com>
2024-12-17 17:11:50 +0600

    course page

--- resources/js/front/pages/course/Courses.vue
+++ resources/js/front/pages/course/Courses.vue
@@ -10,24 +10,31 @@
             <div class="w-100 px-5">
                 <div class="w-100 px-5 position-relative z-2">
                     <div class="breadcrumb-left">
-                        <h1 class="w-100 text-white animate__animated animate__fadeInLeft">
-                            Courses
-                        </h1>
-                        <ul class="breadcrumb w-100 d-flex justify-content-start align-items-center fs-4 animate__animated animate__fadeInLeft">
-                            <li class="breadcrumb-item">
-                                <router-link :to="{name: 'Home'}" class="text-decoration-none text-white p-0 m-0">
-                                    Home
-                                </router-link>
-                            </li>
-                            <li class="text-white mx-3">
-                                /
-                            </li>
-                            <li class="breadcrumb-item">
-                                <router-link :to="{name: 'Courses'}" class="text-decoration-none text-white p-0 m-0">
-                                    Courses
-                                </router-link>
-                            </li>
-                        </ul>
+                        <div class="w-100 mt-5">
+                            <h1 class="w-100 text-white animate__animated animate__fadeInLeft">
+                                Courses
+                            </h1>
+                            <ul class="breadcrumb w-100 d-flex justify-content-start align-items-center fs-4 animate__animated animate__fadeInLeft">
+                                <li class="breadcrumb-item">
+                                    <router-link :to="{name: 'Home'}" class="text-decoration-none text-white p-0 m-0">
+                                        Home
+                                    </router-link>
+                                </li>
+                                <li class="text-white mx-3">
+                                    /
+                                </li>
+                                <li class="breadcrumb-item">
+                                    <router-link :to="{name: 'Courses'}" class="text-decoration-none text-white p-0 m-0">
+                                        Courses
+                                    </router-link>
+                                </li>
+                            </ul>
+
+                            <h1 class="mt-5 text-white fw-bold">
+                                Weekly one-hour <br> online open events <br>
+                                <a class="btn btn-outline-light btn-lg mt-3 px-5">Book Here</a>
+                            </h1>
+                        </div>
                     </div>
                 </div>
             </div>
