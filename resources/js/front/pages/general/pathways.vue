d02cf4c6242b19eabe538dbe2bede895300ec865
Ridwanul Hafiz <ridwanul.hafiz@gmail.com>
2024-12-18 14:42:09 +0600

    update-pathway-ui

--- resources/js/front/pages/general/pathways.vue
+++ resources/js/front/pages/general/pathways.vue
@@ -8,114 +8,166 @@
                 <table class="table table-striped">
                     <thead>
                     <tr>
-                        <th class="fs-2 text-center p-3 text-white" colspan="2">
+                        <th class="fs-2 text-center p-3 text-white rounded-3" colspan="2">
                             Potential Progression Pathways
                         </th>
                     </tr>
                     </thead>
                     <tbody>
                     <tr>
-                        <td class="fs-4 text-secondary px-3">
-                            BSc (Hons) Biochemistry/Biomedical Science
+                        <td class="p-2 ps-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                BSc (Hons) Biochemistry/Biomedical Science
+                            </div>
                         </td>
-                        <td class="fs-4 text-secondary px-3">
-                            BSc (Hons) Nursing (Mental Health)
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="fs-4 text-secondary px-3">
-                            BSc (Hons) Biology
-                        </td>
-                        <td class="fs-4 text-secondary px-3">
-                            BSc (Hons) Nutrition and Human Health
+                        <td class="p-2 pe-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                BSc (Hons) Nursing (Mental Health)
+                            </div>
                         </td>
                     </tr>
                     <tr>
-                        <td class="fs-4 text-secondary px-3">
-                            BSc (Hons) Dental Hygiene
+                        <td class="p-2 ps-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                BSc (Hons) Biology
+                            </div>
                         </td>
-                        <td class="fs-4 text-secondary px-3">
-                            BSc (Hons) Occupational Therapy
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="fs-4 text-secondary px-3">
-                            BSc (Hons) Dental Hygiene and dental therapy
-                        </td>
-                        <td class="fs-4 text-secondary px-3">
-                            BSc (Hons) Operating department practice
+                        <td class="p-2 pe-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                BSc (Hons) Nutrition and Human Health
+                            </div>
                         </td>
                     </tr>
                     <tr>
-                        <td class="fs-4 text-secondary px-3">
-                            BSc (Hons) Diagnostic Radiography
+                        <td class="p-2 ps-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                BSc (Hons) Dental Hygiene
+                            </div>
                         </td>
-                        <td class="fs-4 text-secondary px-3">
-                            BSc (Hons) Pharmacy / pharmaceutical science
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="fs-4 text-secondary px-3">
-                            BSc (Hons) Dietetics and human nutrition
-                        </td>
-                        <td class="fs-4 text-secondary px-3">
-                            BSc (Hons) Paramedic Science / Paramedicine
+                        <td class="p-2 pe-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                BSc (Hons) Occupational Therapy
+                            </div>
                         </td>
                     </tr>
                     <tr>
-                        <td class="fs-4 text-secondary px-3">
-                            BSc (Hons) Healthcare science (Life Sciences)
+                        <td class="p-2 ps-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                BSc (Hons) Dental Hygiene and dental therapy
+                            </div>
                         </td>
-                        <td class="fs-4 text-secondary px-3">
-                            BSc (Hons) Physiotherapy
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="fs-4 text-secondary px-3">
-                            BSc (Hons) Health Studies
-                        </td>
-                        <td class="fs-4 text-secondary px-3">
-                            BSc (Hons) Radiography (Radiotherapy and Oncology)
+                        <td class="p-2 pe-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                BSc (Hons) Operating department practice
+                            </div>
                         </td>
                     </tr>
                     <tr>
-                        <td class="fs-4 text-secondary px-3">
-                            BSc (Hons) Midwifery
+                        <td class="p-2 ps-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                BSc (Hons) Diagnostic Radiography
+                            </div>
                         </td>
-                        <td class="fs-4 text-secondary px-3">
-                            BSc (Hons) Speech and language therapy
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="fs-4 text-secondary px-3">
-                            BSc (Hons) Nursing
-                        </td>
-                        <td class="fs-4 text-secondary px-3">
-                            Trainee Nursing Associate
+                        <td class="p-2 pe-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                BSc (Hons) Pharmacy / pharmaceutical science
+                            </div>
                         </td>
                     </tr>
                     <tr>
-                        <td class="fs-4 text-secondary px-3">
-                            BSc (Hons) Nursing (Adult)
+                        <td class="p-2 ps-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                BSc (Hons) Dietetics and human nutrition
+                            </div>
                         </td>
-                        <td class="fs-4 text-secondary px-3">
-                            Degree Apprenticeships Nursing and/or Trainee Nursing Associate
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="fs-4 text-secondary px-3">
-                            BSc (Hons) Nursing (Child)
-                        </td>
-                        <td class="fs-4 text-secondary px-3">
-                            MSc (combined pathway) Adult and Children's Nursing
+                        <td class="p-2 pe-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                BSc (Hons) Paramedic Science / Paramedicine
+                            </div>
                         </td>
                     </tr>
                     <tr>
-                        <td class="fs-4 text-secondary px-3">
-                            BSc (Hons) Nursing (Learning Disabilities)
+                        <td class="p-2 ps-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                BSc (Hons) Healthcare science (Life Sciences)
+                            </div>
                         </td>
-                        <td class="fs-4 text-secondary px-3">
-                            MSc (combined pathway) Adult and Mental Health Nursing
+                        <td class="p-2 pe-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                BSc (Hons) Physiotherapy
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="p-2 ps-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                BSc (Hons) Health Studies
+                            </div>
+                        </td>
+                        <td class="p-2 pe-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                BSc (Hons) Radiography (Radiotherapy and Oncology)
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="p-2 ps-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                BSc (Hons) Midwifery
+                            </div>
+                        </td>
+                        <td class="p-2 pe-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                BSc (Hons) Speech and language therapy
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="p-2 ps-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                BSc (Hons) Nursing
+                            </div>
+                        </td>
+                        <td class="p-2 pe-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                Trainee Nursing Associate
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="p-2 ps-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                BSc (Hons) Nursing (Adult)
+                            </div>
+                        </td>
+                        <td class="p-2 pe-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                Degree Apprenticeships Nursing and/or Trainee Nursing Associate
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="p-2 ps-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                BSc (Hons) Nursing (Child)
+                            </div>
+                        </td>
+                        <td class="p-2 pe-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                MSc (combined pathway) Adult and Children's Nursing
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="p-2 ps-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                BSc (Hons) Nursing (Learning Disabilities)
+                            </div>
+                        </td>
+                        <td class="p-2 pe-0">
+                            <div class="fs-4 text-secondary p-3 rounded-3">
+                                MSc (combined pathway) Adult and Mental Health Nursing
+                            </div>
                         </td>
                     </tr>
                     </tbody>
