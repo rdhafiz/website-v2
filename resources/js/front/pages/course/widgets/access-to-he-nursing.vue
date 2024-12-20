07485be9f3ad818e6f062feb3d19198b80d9c8da
Ridwanul Hafiz <ridwanul.hafiz@gmail.com>
2024-12-20 10:11:14 +0600

    change-content-of-nuring

--- resources/js/front/pages/course/widgets/access-to-he-nursing.vue
+++ resources/js/front/pages/course/widgets/access-to-he-nursing.vue
@@ -129,21 +129,21 @@
                                 <div class="mb-2">
                                     <div
                                         class="w-100 px-3 py-2 border border-secondary border-opacity-25 rounded-3 shadow mb-4">
-                                        <div class="fs-4 fw-medium">Sociology (3 Credits)</div>
-                                        <div class="fs-6">
-                                            <p>Health Inequalities</p>
-                                        </div>
+                                        <div class="fs-4 fw-medium">Sociology (6 Credits)</div>
+                                        <ul class="fs-6">
+                                            <li>Health Inequalities</li>
+                                            <li>Social Drugs and Medicines</li>
+                                        </ul>
                                     </div>
                                 </div>
                                 <div class="mb-2">
                                     <div
                                         class="w-100 px-3 py-2 border border-secondary border-opacity-25 rounded-3 shadow mb-4">
-                                        <div class="fs-4 fw-medium">Health Studies (6 Credits)</div>
+                                        <div class="fs-4 fw-medium">Health Studies (3 Credits)</div>
                                         <div class="fs-6">
                                             <ul class="m-0">
                                                 <li>Promoting Health</li>
                                                 <li>Actions of Medicines on the Human Body</li>
-                                                <li>Sociology</li>
                                             </ul>
                                         </div>
                                     </div>
