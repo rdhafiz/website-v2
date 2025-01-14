b39a9711aead0c821381ba890227e4d8e6be74e7
Faozul Azim <rafi_ccj@hotmail.com>
2025-01-14 17:43:52 +0600

    update mail template design

--- resources/views/emails/interest-application.blade.php
+++ resources/views/emails/interest-application.blade.php
@@ -5,17 +5,15 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Mediprospects</title>
     <style>
         body {
             font-size : 20px;
         }
-
         .row {
             padding : 7px 0;
             width   : 100%;
         }
-
         .col-left {
             margin-right : 10px;
             font-weight  : bold;
@@ -24,33 +22,35 @@
 </head>
 <body>
 
-    <h3 style="text-align: center"><b>{{ $input['name'] }}</b> has expressed <br> interest for <b>{{$input['course_name']}}</b></h3>
+    <p style="text-align: center;">
+        <strong>{{ $input['name'] }}</strong> has expressed interest for <strong>{{$input['course_name']}}</strong> course.
+    </p>
     <br><br>
 
     <table style="width: 100%;border-spacing: 0;">
         <tr>
-            <th style="text-align: right;border: 1px solid #e6e6e6;padding: 10px;background-color: #f9f9f9;width: 35%;">Interested for the Course:</th>
-            <td style="text-align: left;border: 1px solid #e6e6e6;padding: 10px;background-color: #f9f9f9;">{{$input['course_name']}}</td>
+            <th style="text-align: right;padding: 10px;background-color: #f9f9f9;width: 35%;">Interested for the Course:</th>
+            <td style="text-align: left;padding: 10px;background-color: #f9f9f9;">{{$input['course_name']}}</td>
         </tr>
         <tr>
-            <th style="text-align: right;border: 1px solid #e6e6e6;padding: 10px;background-color: #f9f9f9;width: 35%;">Name:</th>
-            <td style="text-align: left;border: 1px solid #e6e6e6;padding: 10px;background-color: #f9f9f9;">{{ $input['name'] }}</td>
+            <th style="text-align: right;padding: 10px;background-color: #f9f9f9;width: 35%;">Name:</th>
+            <td style="text-align: left;padding: 10px;background-color: #f9f9f9;">{{ $input['name'] }}</td>
         </tr>
         <tr>
-            <th style="text-align: right;border: 1px solid #e6e6e6;padding: 10px;background-color: #f9f9f9;width: 35%;">Phone:</th>
-            <td style="text-align: left;border: 1px solid #e6e6e6;padding: 10px;background-color: #f9f9f9;">{{$input['contact_number']}}</td>
+            <th style="text-align: right;padding: 10px;background-color: #f9f9f9;width: 35%;">Phone:</th>
+            <td style="text-align: left;padding: 10px;background-color: #f9f9f9;">{{$input['contact_number']}}</td>
         </tr>
         <tr>
-            <th style="text-align: right;border: 1px solid #e6e6e6;padding: 10px;width: 35%;">Email:</th>
-            <td style="text-align: left;border: 1px solid #e6e6e6;padding: 10px;">{{$input['email']}}</td>
+            <th style="text-align: right;padding: 10px;background-color: #f9f9f9;width: 35%;">Email:</th>
+            <td style="text-align: left;padding: 10px;background-color: #f9f9f9;">{{$input['email']}}</td>
         </tr>
         <tr>
-            <th style="text-align: right;border: 1px solid #e6e6e6;padding: 10px;width: 35%;">Preferred method of communication:</th>
-            <td style="text-align: left;border: 1px solid #e6e6e6;padding: 10px;">{{$input['preferred_communication_method']}}</td>
+            <th style="text-align: right;padding: 10px;background-color: #f9f9f9;width: 35%;">Preferred method of communication:</th>
+            <td style="text-align: left;padding: 10px;background-color: #f9f9f9;">{{$input['preferred_communication_method']}}</td>
         </tr>
         <tr>
-            <th style="text-align: right;border: 1px solid #e6e6e6;padding: 10px;background-color: #f9f9f9;width: 35%;">Postcode:</th>
-            <td style="text-align: left;border: 1px solid #e6e6e6;padding: 10px;background-color: #f9f9f9;">
+            <th style="text-align: right;padding: 10px;background-color: #f9f9f9;width: 35%;">Postcode:</th>
+            <td style="text-align: left;padding: 10px;background-color: #f9f9f9;">
                 {{$input['postcode']}}
             </td>
         </tr>
