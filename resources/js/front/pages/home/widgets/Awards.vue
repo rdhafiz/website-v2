8f8dde9aff0e54294d496c2771e2de1f02a81cd9
Faozul Azim <rafi_ccj@hotmail.com>
2025-01-21 16:09:36 +0600

    update ui

--- resources/js/front/pages/home/widgets/Awards.vue
+++ resources/js/front/pages/home/widgets/Awards.vue
@@ -26,9 +26,9 @@
                                                     <img class="person-image rounded-circle" :src="'/assets/img/award/lesley-heaney.jpg'" alt="lesley-heaney" loading="lazy" />
                                                 </div>
                                                 <div class="col-12 col-xl-7 award-description">
-                                                    <h1 class="h1-lg text-white text-shadow-sm">Learner of the Year</h1>
-                                                    <h2 class="text-white text-shadow-sm">Recognizing Academic Excellence</h2>
-                                                    <p class="fs-4 text-white text-shadow-sm">Awarded to a learner who has demonstrated exceptional dedication, achievement, and progress in their studies.</p>
+                                                    <h1 class="h1-lg text-white">Learner of the Year</h1>
+                                                    <h2 class="text-white">Recognizing Academic Excellence</h2>
+                                                    <p class="fs-4 text-white">Awarded to a learner who has demonstrated exceptional dedication, achievement, and progress in their studies.</p>
                                                     <p class="">
 <!--                                                        <a class="btn btn-lg btn-outline-light px-4"> Learn More <i class="fas fa-arrow-right ms-3"></i></a>-->
                                                     </p>
@@ -50,9 +50,9 @@
                                                     <img class="person-image rounded-circle" :src="'/assets/img/award/toy.png'" alt="toy">
                                                 </div>
                                                 <div class="col-xl-7 award-description">
-                                                    <h1 class="h1-lg text-white text-shadow-sm">Teacher of the Year</h1>
-                                                    <h2 class="text-white text-shadow-sm">Inspiring the Future</h2>
-                                                    <p class="fs-4 text-white text-shadow-sm">Presented to an educator who has shown exemplary teaching, mentorship, and commitment to student success.</p>
+                                                    <h1 class="h1-lg text-white">Teacher of the Year</h1>
+                                                    <h2 class="text-white">Inspiring the Future</h2>
+                                                    <p class="fs-4 text-white">Presented to an educator who has shown exemplary teaching, mentorship, and commitment to student success.</p>
                                                     <p class="">
 <!--                                                        <a class="btn btn-lg btn-outline-light px-4"> Learn More <i class="fas fa-arrow-right ms-3"></i></a>-->
                                                     </p>
@@ -74,9 +74,9 @@
                                                     <img class="person-image rounded-circle" :src="'/assets/img/award/gemma-lee-award-04.jpg'" alt="gemma-lee-award-04">
                                                 </div>
                                                 <div class="col-xl-7 award-description">
-                                                    <h1 class="h1-lg text-white text-shadow-sm">Keith Fletcher <br> Memorial Award</h1>
-                                                    <h2 class="text-white text-shadow-sm">Excellence in Access Education</h2>
-                                                    <p class="fs-4 text-white text-shadow-sm">Honors learners who have excelled in Access to Higher Education courses, named in memory of Keith Fletcher, a pioneer in this field.</p>
+                                                    <h1 class="h1-lg text-white">Keith Fletcher <br> Memorial Award</h1>
+                                                    <h2 class="text-white">Excellence in Access Education</h2>
+                                                    <p class="fs-4 text-white">Honors learners who have excelled in Access to Higher Education courses, named in memory of Keith Fletcher, a pioneer in this field.</p>
                                                     <p class="">
 <!--                                                        <a class="btn btn-lg btn-outline-light px-4"> Learn More <i class="fas fa-arrow-right ms-3"></i></a>-->
                                                     </p>
@@ -98,9 +98,9 @@
                                                     <img class="person-image rounded-circle" :src="'/assets/img/award/elena.jpg'" alt="elena">
                                                 </div>
                                                 <div class="col-xl-7 award-description">
-                                                    <h1 class="h1-lg text-white text-shadow-sm">Midwifery Student <br> of the Year</h1>
-                                                    <h2 class="text-white text-shadow-sm">Compassion and Commitment</h2>
-                                                    <p class="fs-4 text-white text-shadow-sm">Celebrates a midwifery student who has gone above and beyond in their training and care for others.</p>
+                                                    <h1 class="h1-lg text-white">Midwifery Student <br> of the Year</h1>
+                                                    <h2 class="text-white">Compassion and Commitment</h2>
+                                                    <p class="fs-4 text-white">Celebrates a midwifery student who has gone above and beyond in their training and care for others.</p>
                                                     <p class="">
 <!--                                                        <a class="btn btn-lg btn-outline-light px-4"> Learn More <i class="fas fa-arrow-right ms-3"></i></a>-->
                                                     </p>
